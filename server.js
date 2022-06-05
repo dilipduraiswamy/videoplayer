38d8a05eb3fe923d061158ef1c34e9d0f472533c
dilipduraiswamy <dilipduraiswamy@gmail.com>
2022-06-05 19:52:26 +0530

    test

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/videoplayer'));
+app.use(express.static(__dirname + '/dist/myvideoplayerin'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/videoplayer/index.html'));
+res.sendFile(path.join(__dirname+'/dist/myvideoplayerin/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
