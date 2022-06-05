b9fe68c406233f8459bc6d82523941bfd4b9dc48
dilipduraiswamy <dilipduraiswamy@gmail.com>
2022-06-05 20:33:13 +0530

    test

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+// //Install express server
+// const express = require('express');
+// const path = require('path');
 
-const app = express();
+// const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/myvideoplayerin'));
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist/myvideoplayerin'));
 
-app.get('/*', function(req,res) {
+// app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/myvideoplayerin/index.html'));
-});
+// res.sendFile(path.join(__dirname+'/dist/myvideoplayerin/index.html'));
+// });
 
-// Start the app by listening on the default Heroku port
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
+//Install express server    
+const express = require('express');
+
+const path = require('path');   
+
+const app = express();   
+
+// Serve only the static files form the dist directory    
+app.use(express.static(__dirname + '/dist/videoplayer'));
+
+app.get('/*', function(req,res) {  
+    res.sendFile(path.join(__dirname+'/dist/videoplayer/index.html'));   
+});  
+
+// Start the app by listening on the default Heroku port    
 app.listen(process.env.PORT || 8080);
